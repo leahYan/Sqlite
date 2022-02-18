5902ee93cc48a4161f6f9acb2867d94843fd93e7
leahYan <55049693+leahYan@users.noreply.github.com>
2022-02-18 14:57:39 +1000

    Add read write permission

--- app/src/main/java/com/jovanovic/stefan/sqlitetutorial/SenddataActivity.java
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/SenddataActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class SenddataActivity extends AppCompatActivity {
 
@@ -17,6 +22,7 @@ public class SenddataActivity extends AppCompatActivity {
     CustomAdapter customAdapter;
     MyDatabaseHelper myDB;  //initialise class object
     SQLiteDatabase sqLiteDatabase;
+    File attachment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +36,81 @@ public class SenddataActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent it = new Intent(Intent.ACTION_SEND);
+
                 sqLiteDatabase = myDB.getReadableDatabase();
+
+                copyFileToExternal("Sample.db");
+                sendEmail("RTAData@Riotinto.com");
+/*
+                File root = Environment.getExternalStorageDirectory();
+                String fileName = "Sample.db";
+                if (root.canWrite()) {
+                    attachment = new File(root, fileName);
+                }
+
                 it.setData(Uri.parse("mailto:")); // only email apps should handle this
                 it.putExtra(Intent.EXTRA_EMAIL, new String[]{"RTAData@Riotinto.com"});
                 it.putExtra(Intent.EXTRA_SUBJECT,"Test email");
                 it.putExtra(Intent.EXTRA_TEXT,eMsg.getText());
-                it.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(sqLiteDatabase));
-                it.setType("message/rfc822");
+                //it.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(sqLiteDatabase));
+                it.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(attachment));
+
+                //it.setType("message/rfc822");
                 startActivity(Intent.createChooser(it,"Choose Mail App"));
+                */
+
             }
         });
 
+    }
+
+
+    private  File copyFileToExternal(String fileName) {
+        File file = null;
+        String newPath = Environment.getExternalStorageState();
+
+        try {
+            File f = new File(newPath);
+            f.mkdirs();
+            FileInputStream fin = openFileInput(fileName);
+            FileOutputStream fos = new FileOutputStream(newPath + fileName);
+            byte[] buffer = new byte[1024];
+            int len1 = 0;
+            while ((len1 = fin.read(buffer)) != -1) {
+                fos.write(buffer, 0, len1);
+            }
+            fin.close();
+            fos.close();
+            file = new File(newPath + fileName);
+            if (file.exists())
+                return file;
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
+
+    private void sendEmail(String email) {
+
+        File file = new File(Environment.getExternalStorageState()+"/Download/" + "Sample.db");
+        Uri path = Uri.fromFile(file);
+        Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+        intent.setType("application/octet-stream");
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Test email");
+        String to[] = { email };
+        intent.putExtra(Intent.EXTRA_EMAIL, to);
+        intent.putExtra(Intent.EXTRA_TEXT, Environment.getExternalStorageState());
+        intent.putExtra(Intent.EXTRA_STREAM, path);
+        startActivityForResult(Intent.createChooser(intent, "Send mail..."),
+                1222);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1222) {
+            File file = new File(Environment.getExternalStorageState()+"/Download/" +"Sample.db");
+            file.delete();
+        }
     }
 }
 
